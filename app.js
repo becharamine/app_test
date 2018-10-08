50559117a471f7bdda3920e1114ad6497591dacf
BECHAR Amine <amine.bechar.info@gmail.com>
2018-10-08 10:31:21 +0100

    Ajout des fichiers

---
+++ app.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var fs = require('fs');
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('checkName', function(nom){
+        socket.nom = nom;
+        console.log(nom+'');
+        socket.broadcast.emit('connectionMessage', nom); 
+
+    });
+
+    var callback = function(message){
+        socket.broadcast.emit('message', socket.nom, message);
+    };
+    socket.on('sendMessage', callback);
+});
+
+
+server.listen(8086);
